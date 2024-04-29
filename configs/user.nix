c2cc7c069728975071aada6cb898c309b882232a
TheColorman <thoren.alex@gmail.com>
2024-04-29 20:06:43 +0200

    44 current  2024-04-29 19:16:02  24.05pre616726.7bb2ccd8cdc4  6.6.28                          *

--- configs/user.nix
+++ configs/user.nix
@@ -75,4 +75,24 @@
       };
     };
   };
+
+  # Git config
+  programs.git.config = {
+    url = {
+      "https://github.com/" = {
+        insteadOf = [
+          "gh:"
+          "github:"
+        ];
+      };
+    };
+    user = {
+      email = "thoren.alex@gmail.com";
+      name = "TheColorman";
+      signingKey = "3ECF7505E982C8F6";
+    };
+    commit = {
+      gpgsign = true;
+    };
+  };
 }
