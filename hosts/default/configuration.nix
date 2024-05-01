3024392d25593963641680aff6544fe8b99cd77d
TheColorman <thoren.alex@gmail.com>
2024-05-01 22:49:02 +0200

    57 current  2024-05-01 22:48:55  24.05.20240427.58a1abd       6.6.28                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -13,8 +13,9 @@
     ../../modules/nixos/locale.nix # Danish UTF-8 locale for rendering
     ../../modules/nixos/networking.nix # Networkmanager
     ../../modules/nixos/nix.nix # General nixos config
+    ../../modules/nixos/shell.nix # Global zsh, might make this user-based for home-manager management
+    ../../modules/nixos/snix.nix # Script used to version control nix config
     # Legacy modules
-    ../../modules/nixos/legacy/system.nix
     ../../modules/nixos/legacy/user.nix
   ];
 
