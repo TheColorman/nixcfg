05bb6806d167afe132e88bbbb2ee1171e206459b
TheColorman <thoren.alex@gmail.com>
2024-05-02 23:02:32 +0200

    Rename: default to colornix to match hostname

--- flake.nix
+++ flake.nix
@@ -20,10 +20,10 @@
     nixpkgs,
     ...
   } @ inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.colornix = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
-        ./hosts/default/configuration.nix
+        ./hosts/colornix/configuration.nix
         inputs.home-manager.nixosModules.default
       ];
     };
