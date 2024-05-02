32ad2304f700375f30bebc1cdf3d7e3c5871f8f6
TheColorman <thoren.alex@gmail.com>
2024-05-02 15:37:47 +0200

    Non-config change

--- flake.nix
+++ flake.nix
@@ -8,6 +8,11 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nix-alien = {
+      url = "github:thiagokokada/nix-alien";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
