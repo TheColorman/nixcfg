6d4d3adffc1b3d8d7858e6c933a5fa3df7b54313
TheColorman <thoren.alex@gmail.com>
2024-05-06 16:59:54 +0200

    trying to extract host-level flake configs

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -18,22 +19,31 @@
       url = "path:./modules/nixos/fw-ectool";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # configurations
+    colornix.url = "path:./hosts/colornix";
   };
 
   outputs = {
     self,
     nixpkgs,
+    nixos-hardware,
     fw-ectool,
+    colornix,
     ...
   } @ inputs: {
-    nixosConfigurations.colornix = nixpkgs.lib.nixosSystem rec {
-      system = "x86_64-linux";
-      specialArgs = {inherit inputs system;};
-
-      modules = [
-        ./hosts/colornix/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
+    #nixosConfigurations.colornix = nixpkgs.lib.nixosSystem rec {
+    #  system = "x86_64-linux";
+    #  specialArgs = {inherit inputs system;};
+    #
+    #  modules = [
+    #    ./hosts/colornix/configuration.nix
+    #    inputs.home-manager.nixosModules.default
+    #    nixos-hardware.nixosModules.framework-13-7040-amd
+    #  ];
+    #};
+    nixosConfigurations = {
+      inherit colornix;
     };
   };
 }
