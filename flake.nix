c57f88ae155ab88bbe7dea3bc060ec3bdd0d7088
TheColorman <thoren.alex@gmail.com>
2024-05-07 23:46:53 +0200

    Remove: old code

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    #nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -15,34 +14,19 @@
     #  inputs.nixpkgs.follows = "nixpkgs";
     #};
 
-    #fw-ectool = {
-    #  url = "path:./modules/nixos/fw-ectool";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
-
     # configurations
+    # TODO: dynamically import host configurations
     colornix.url = "path:./hosts/colornix";
+    colornix.inputs.home-manager.follows = "home-manager";
   };
 
   outputs = {
     self,
     nixpkgs,
     home-manager,
-    #nixos-hardware,
-    #fw-ectool,
     colornix,
     ...
   } @ inputs: {
-    #nixosConfigurations.colornix = nixpkgs.lib.nixosSystem rec {
-    #  system = "x86_64-linux";
-    #  specialArgs = {inherit inputs system;};
-    #
-    #  modules = [
-    #    ./hosts/colornix/configuration.nix
-    #    inputs.home-manager.nixosModules.default
-    #    nixos-hardware.nixosModules.framework-13-7040-amd
-    #  ];
-    #};
     nixosConfigurations = {
       colornix = nixpkgs.lib.nixosSystem colornix.config;
     };
