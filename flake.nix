2ae5aba139ba34ec9572326263a3bd063545cf74
TheColorman <thoren.alex@gmail.com>
2024-05-21 21:05:36 +0200

    Use local nixpkgs for main flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "/home/color/nixos/nixpkgs?ref=nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
