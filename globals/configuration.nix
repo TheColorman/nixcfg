7ab3350f578fb790fdc20fcb73047557a7a8ca96
TheColorman <thoren.alex@gmail.com>
2024-05-21 21:48:33 +0200

    Temp fwupdate stuff for fingerprint reader

--- globals/configuration.nix
+++ globals/configuration.nix
@@ -1,4 +1,8 @@
 {...}: {
+  environment.systemPackages = with pkgs; [
+    wget
+  ];
+
   users.mutableUsers = false;
   system.nixos.label = "add_tmux";
 }
--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -25,10 +25,18 @@ in {
     input-remapper
     aria2
     (pkg "fw-ectool")
+    fprintd
   ];
 
   environment.pathsToLink = ["/share/zsh"];
   services.input-remapper.enable = true;
   services.fwupd.enable = true;
+  # we need fwupd 1.9.7 to downgrade the fingerprint sensor firmware
+  services.fwupd.package = (import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/bb2009ca185d97813e75736c2b8d1d8bb81bde05.tar.gz";
+    sha256 = "sha256:003qcrsq5g5lggfrpq31gcvj82lb065xvr7bpfa8ddsw8x4dnysk";
+  }) {
+    inherit (pkgs) system;
+  }).fwupd;
   hardware.bluetooth.enable = true;
 }
