a59d56163b2eb9d0cd118d6efa8bd8fed5766114
TheColorman <thoren.alex@gmail.com>
2024-05-21 22:52:07 +0200

    Update

--- hosts/framework/flake.nix
+++ hosts/framework/flake.nix
@@ -37,7 +37,7 @@
         "nix" # General nixos config
         "shell" # Global zsh, might make this user-based for home-manager management
         "snix" # Script used to version control nix config
-        "fonts/caskadiacode"
+        "fonts/cascadiacode"
       ];
     };
   };
