e43a75681750903060b3dbec46085258ddfb31b3
TheColorman <github@colorman.me>
2024-08-06 09:25:51 +0200

    "Update deps"

--- globals/configuration.nix
+++ globals/configuration.nix
@@ -4,5 +4,5 @@
   ];
 
   users.mutableUsers = false;
-  system.nixos.label = "add_syncthing_colorphone";
+  system.nixos.label = "update_deps";
 }
--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -2,7 +2,7 @@
   # Patch for connecting to legacy eduroam networks
   nixpkgs.config.packageOverrides = pkgs: rec {
     wpa_supplicant = pkgs.wpa_supplicant.overrideAttrs (oldAttrs: {
-      patches = oldAttrs.patches ++ [./patches/eduroam.patch];
+      patches = (oldAttrs.patches or []) ++ [./patches/eduroam.patch];
     });
   };
 
