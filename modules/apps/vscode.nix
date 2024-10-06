8dfb4cfbc5dd6c72137c13b91c74e333b7e3e9c4
TheColorman <github@colorman.me>
2024-10-06 21:54:34 +1100

    Don't install vscode through home-manager

--- modules/apps/vscode.nix
+++ modules/apps/vscode.nix
@@ -1,8 +1,9 @@
-{ config, ... }: {
-  home-manager.users."${config.my.username}".programs.vscode = {
-    enable = true;
+{ config, pkgs, ... }: {
+  users.users.${config.my.username}.programs = with pkgs; [ vscode ];
+ #  home-manager.users."${config.my.username}".programs.vscode = {
+ #    enable = true;
 
     # extensions = []; # @TODO
-    mutableExtensionsDir = true;
-  };
+ #    mutableExtensionsDir = true;
+ #  };
 }
