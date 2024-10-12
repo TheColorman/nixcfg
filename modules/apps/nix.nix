e9ca690d6f09b31e85c571aec85aa6c07e40a771
TheColorman <github@colorman.me>
2024-10-12 23:42:37 +1100

    Remove frankly annoying git shit from nix build scripts

--- modules/apps/nix.nix
+++ modules/apps/nix.nix
@@ -1,51 +1,13 @@
-# @TODO: this shit barely works
-
 { pkgs, ... }:
 let
-  inherit (pkgs) writeShellApplication;
-  preBuild = ''
-    pushd ~/nixcfg
-    git add .
-    git commit -m "tnix-temp"
-  '';
-  postBuild = ''
-    git reset HEAD~1
-    popd
-  '';
+  script = name: text: pkgs.writeShellApplication { inherit name text; };
 in
 {
   environment.systemPackages = [
-    # These three scripts are for testing unstaged config changes
-    (writeShellApplication {
-      name = "tnix";
-      runtimeInputs = with pkgs; [ git ];
-      text = preBuild + ''
-        sudo nixos-rebuild test --flake ~/nixcfg --option eval-cache false
-      '' + postBuild;
-    })
-    (writeShellApplication {
-      name = "dbnix";
-      runtimeInputs = with pkgs; [ git ];
-      text = preBuild + ''
-        sudo nixos-rebuild dry-build --flake ~/nixcfg --option eval-cache false
-      '' + postBuild;
-    })
-    (writeShellApplication {
-      name = "danix";
-      runtimeInputs = with pkgs; [ git ];
-      text = preBuild + ''
-        sudo nixos-rebuild dry-activate --flake ~/nixcfg
-      '' + postBuild;
-    })
-    # These are for actually adding a generation to the nix store, they assume there is already a commit
-    # @TODO: Eventually I should create a script that runs git commit in case there are unstaged changes
-    (writeShellApplication {
-      name = "snix";
-      text = "sudo nixos-rebuild switch --flake ~/nixcfg";
-    })
-    (writeShellApplication {
-      name = "bnix";
-      text = "sudo nixos-rebuild boot --flake ~/nixcfg";
-    })
+    (script "tnix"  "sudo nixos-rebuild test --flake ~/nixcfg --option eval-cache false")
+    (script "dbnix" "sudo nixos-rebuild dry-build --flake ~/nixcfg --option eval-cache false")
+    (script "danix" "sudo nixos-rebuild dry-activate --flake ~/nixcfg")
+    (script "snix"  "sudo nixos-rebuild switch --flake ~/nixcfg")
+    (script "bnix"  "sudo nixos-rebuild boot --flake ~/nixcfg")
   ];
 }
