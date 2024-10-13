2872f33be5a7e5ee2209cbd67bd43bbafb277be2
TheColorman <github@colorman.me>
2024-10-13 23:35:40 +1100

    Add self as trusted users

--- modules/profiles/common.nix
+++ modules/profiles/common.nix
@@ -1,28 +1,31 @@
 # Common configuration, assumed to be imported in all hosts
-{ lib, pkgs, inputs, config, ... }:
-let
-  username = config.my.username;
-in
 {
-  imports = [ inputs.home-manager.nixosModules.default ];
+  lib,
+  inputs,
+  config,
+  ...
+}: let
+  username = config.my.username;
+in {
+  imports = [inputs.home-manager.nixosModules.default];
 
-  options.my.username = lib.mkOption
-    {
-      default = "color";
-      description = ''
-        Username used for home-manager configuration.
-        It's used in custom modules to allow them to be imported by any user. This module is used as a dependency of any module that requires home-manager.
-      '';
-    };
+  options.my.username = lib.mkOption {
+    default = "color";
+    description = ''
+      Username used for home-manager configuration.
+      It's used in custom modules to allow them to be imported by any user. This module is used as a dependency of any module that requires home-manager.
+    '';
+  };
 
   config = {
+    # flakification
+    nix.settings = {
+      experimental-features = ["nix-command" "flakes"];
+      trusted-users = ["root" username];
+    };
+
     # Allow execution of dynamic binaries
     programs.nix-ld.enable = true;
-    programs.nix-ld.libraries = with pkgs; [
-      # Add any missing dynamic libraries for unpackaged
-      # programs here, NOT in environment.systemPackages
-    ];
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
     nixpkgs.config.allowUnfree = true;
 
     # Dynamic symlinks in /bin, useful for shebangs
