23f855760ee3f89eb149e9736059b2fa1fb05c86
TheColorman <github@colorman.me>
2024-10-13 21:20:31 +1100

    Add nh

---
+++ modules/apps/nh.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }: let
+  user = config.my.username;
+in {
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 30d --keep 50";
+    flake = "/home/${user}/nixcfg";
+  };
+  environment.systemPackages = [ pkgs.nh ];
+}
