2344ae61272c0ed4f3bab9ab6a4da36fe4574d02
TheColorman <github@colorman.me>
2024-10-13 22:12:15 +1100

    Update nix helper scripts

--- modules/apps/nix.nix
+++ modules/apps/nix.nix
@@ -1,14 +1,40 @@
-{ pkgs, outputs, ... }:
+{ pkgs, outputs, lib, config, ... }:
 let
+  inherit (lib.meta) getExe;
+  user = config.my.username;
   script = name: text: pkgs.writeShellApplication { inherit name text; };
+  flakedir = "/home/${user}/nixcfg";
+
+  gitTagScript = ''
+    generation=$(nix-env --list-generations | grep current | awk '{print $1}')
+    echo "Built generation $generation."
+    read -r -p "Create git tag? [Y/n]: " choice
+
+    choice=''${choice:-Y}
+
+    if [[ "$choice" == [Yy] ]]; then
+      pushd ${flakedir} > /dev/null
+      ${getExe pkgs.git} tag -a "$generation" -m "Build $generation"
+      ${getExe pkgs.git} push --tag
+      popd > /dev/null
+    fi
+  '';
 in
 {
   imports = [ outputs.modules.apps-nh ];
   environment.systemPackages = [
-    (script "tnix"  "nh os test --verbose")
-    (script "dbnix" "nh os build --dry")
-    (script "danix" "nh os test --dry")
-    (script "snix"  "nh os switch")
-    (script "bnix"  "nh os build")
+    (script "tnix"  "nh os test --verbose ${flakedir}")
+    (script "dbnix" "nh os build --dry ${flakedir}")
+    (script "danix" "nh os test --dry ${flakedir}")
+    (script "bnix"  ''
+      nh os boot ${flakedir}
+
+      ${gitTagScript}
+    '')
+    (script "snix" ''
+      nh os switch ${flakedir}
+
+      ${gitTagScript} 
+    '')
   ];
 }
