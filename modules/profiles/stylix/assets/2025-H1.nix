abcb444455c6225a92a9f93878ed1a6c238cc8dd
TheColorman <github@colorman.me>
2024-12-29 21:47:06 +1100

    Stylix: Add 2025-H1 style

---
+++ modules/profiles/stylix/assets/2025-H1.nix
@@ -0,0 +1,39 @@
+{
+  dark = {
+    base00 = "0f3037";
+    base01 = "473127";
+    base02 = "7d92ae";
+    base03 = "9f4d25";
+    base04 = "dd9c93";
+    base05 = "d3e6e6";
+    base06 = "e3e6e6";
+    base07 = "e6e6e6";
+    base08 = "e3868e";
+    base09 = "feceba";
+    base0A = "ffeab9";
+    base0B = "d1ffb9";
+    base0C = "b7dce2";
+    base0D = "b9c9fc";
+    base0E = "e7b9fc";
+    base0F = "d69b79";
+  };
+  # @TODO: create light mode scheme
+  light = {
+    base00 = "000000";
+    base01 = "000000";
+    base02 = "000000";
+    base03 = "000000";
+    base04 = "000000";
+    base05 = "000000";
+    base06 = "000000";
+    base07 = "000000";
+    base08 = "000000";
+    base09 = "000000";
+    base0A = "000000";
+    base0B = "000000";
+    base0C = "000000";
+    base0D = "000000";
+    base0E = "000000";
+    base0F = "000000";
+  };
+}
