bc2243d4e8fc3bb8a90740da07f771fb4af3666f
TheColorman <github@colorman.me>
2025-01-02 16:17:49 +1100

    Update light mode color theme

--- modules/profiles/stylix/assets/2025-H1.nix
+++ modules/profiles/stylix/assets/2025-H1.nix
@@ -19,21 +19,21 @@
   };
   # @TODO: create light mode scheme
   light = {
-    base00 = "000000";
-    base01 = "000000";
-    base02 = "000000";
-    base03 = "000000";
-    base04 = "000000";
-    base05 = "000000";
-    base06 = "000000";
-    base07 = "000000";
-    base08 = "000000";
-    base09 = "000000";
-    base0A = "000000";
-    base0B = "000000";
-    base0C = "000000";
-    base0D = "000000";
-    base0E = "000000";
-    base0F = "000000";
+    base00 = "c6f3f7";
+    base01 = "fcccb6";
+    base02 = "7689a4";
+    base03 = "ff7b3b";
+    base04 = "9f4d25";
+    base05 = "578484";
+    base06 = "404242";
+    base07 = "e6e6e6";
+    base08 = "e3868e";
+    base09 = "feceba";
+    base0A = "ffeab9";
+    base0B = "d1ffb9";
+    base0C = "b7dce2";
+    base0D = "b9c9fc";
+    base0E = "e7b9fc";
+    base0F = "d69b79";
   };
 }
