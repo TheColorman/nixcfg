b50eeee61f117f2ec966658850b3e5340d95f38e
TheColorman <github@colorman.me>
2025-01-21 22:54:07 +0900

    Update nixpkgs input to point to unstable again

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "Colorman NixOS configuration flake";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/3d9b0a86d74e9dc3a85ed8d2abcef3ab0940c69d";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware?ref=master";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
