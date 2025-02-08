cf6c0837126d08ffde2a540129d2bcbe6dcf1285
TheColorman <github@colorman.me>
2025-02-09 06:35:16 +1100

    Fix wsl again i guess

--- hosts/boarding/configuration.nix
+++ hosts/boarding/configuration.nix
@@ -4,6 +4,7 @@
   pkgs,
   outputs,
   inputs,
+  lib,
   ...
 }: let
   username = "boarder";
@@ -26,9 +27,36 @@ in {
   my.username = username;
   networking.hostName = "boarding";
 
-  wsl.enable = true;
-  wsl.defaultUser = username;
-  wsl.docker-desktop.enable = true;
+  wsl = {
+    enable = true;
+    wslConf.automount.root = "/mnt";
+    wslConf.interop.appendWindowsPath = false;
+    wslConf.network.generateHosts = false;
+    defaultUser = username;
+    startMenuLaunchers = true;
+
+    docker-desktop.enable = false; # done manually
+
+    extraBin = with pkgs; [
+      # Binaries for Docker Desktop wsl-distro-proxy
+      {src = "${coreutils}/bin/mkdir";}
+      {src = "${coreutils}/bin/cat";}
+      {src = "${coreutils}/bin/whoami";}
+      {src = "${coreutils}/bin/ls";}
+      {src = "${busybox}/bin/addgroup";}
+      {src = "${su}/bin/groupadd";}
+      {src = "${su}/bin/usermod";}
+    ];
+  };
+
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+    autoPrune.enable = true;
+  };
+
+  ## patch the script
+  systemd.services.docker-desktop-proxy.script = lib.mkForce ''${config.wsl.wslConf.automount.root}/wsl/docker-desktop/docker-desktop-user-distro proxy --docker-desktop-root ${config.wsl.wslConf.automount.root}/wsl/docker-desktop "C:\Program Files\Docker\Docker\resources"'';
 
   users.users."${username}" = {
     isNormalUser = true;
