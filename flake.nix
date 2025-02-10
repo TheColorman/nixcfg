5068966f194b8fc67d13b00ae9da287ee8cf1721
TheColorman <github@colorman.me>
2025-02-11 02:30:57 +1100

    Pin nixpkgs to specific commit

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Colorman NixOS configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=18536bf";
     nixpkgs-24-05.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware?ref=master";
     home-manager.url = "github:nix-community/home-manager";
