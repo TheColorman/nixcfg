47c151f08faa2bee94ca4caa99b07d028cfda4ae
TheColorman <github@colorman.me>
2025-02-11 16:07:00 +0900

    switch timezone

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -84,7 +84,7 @@ in {
   services.power-profiles-daemon.enable = true;
   hardware.bluetooth.enable = true;
 
-  time.timeZone = "Asia/Tokyo";
+  time.timeZone = "Europe/Copenhagen";
   i18n.defaultLocale = "en_DK.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "da_DK.UTF-8";
