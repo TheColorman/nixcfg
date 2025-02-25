de6e96bda6f57e72920800b24788cb9dd96291cb
TheColorman <colorman@github.com>
2025-02-26 01:03:33 +1100

    Update nixpkgs to known good commit

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # == Important modules ==
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/5df43628fdf08d642be8ba5b3625a6c70731c19c";
+    nixpkgs.url = "github:nixos/nixpkgs/8bb37161a0488b89830168b81c48aed11569cb93";
     nixos-hardware.url = "github:NixOS/nixos-hardware?ref=master";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
