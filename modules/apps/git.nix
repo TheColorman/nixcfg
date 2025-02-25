f7eea6f451b1abc7a26db796d489467351a6ea21
TheColorman <colorman@github.com>
2025-02-26 01:54:46 +1100

    Apps/Git: switch to home-manager

--- modules/apps/git.nix
+++ modules/apps/git.nix
@@ -1,56 +1,68 @@
-{ pkgs, ... }:
-{
+{ pkgs, config, ... }:
+let
+  user = config.my.username;
+in {
   # Multiple modules rely on apps-gpg, so it is instead imported in host
   # configuration.nix.
   # See: https://github.com/NixOS/nixpkgs/issues/340361
   # imports = [ outputs.modules.apps-gpg ];
 
-  environment.systemPackages = with pkgs; [ git gh ];
-  programs.git = {
-    config = {
-      user = {
-        email = "github@colorman.me";
-        name = "TheColorman";
-        signingKey = "AB110475B417291D"; # @TODO: Can this key be created dynamically?
+  home-manager.users.${user}.programs = {
+    git = {
+      enable = true;
+      aliases = {
+        a = "add";
+        c = "commit";
+        co = "checkout";
+        s = "switch";
       };
-      diff = {
-        algorithm = "histogram";
-        colorMoved = "plain";
-        mnemonicPrefix = true;
-        renames = true;
-      };
-      commit.verbose = true;
-      pull.rebase = true;
-      push = {
-        default = "simple";
-        autoSetupRemote = true;
-        followTags = true;
-      };
-      merge.conflictstyle = "zdiff3";
-      column.ui = "auto";
-      branch.sort = "-comitterdate";
-      tag.sort = "version:refname";
-      init.defaultBranch = "main";
-      fetch = {
-        prune = true;
-        pruneTags = true;
-        all = true;
-      };
-      help.autocorrect = "prompt";
-      rerere = {
-        enabled = true;
-        autoupdate = true;
-      };
-      core.excludesfile = "~/.gitignore";
-      rebase = {
-        autoSquash = true;
-        autoStash = true;
-        updateRefs = true;
-        fsmonitor = true;
-      };
-      commit = {
-        gpgsign = true;
+      extraConfig = {
+        diff = {
+          algorithm = "histogram";
+          colorMoved = "plain";
+          mnemonicPrefix = true;
+          renames = true;
+        };
+        commit.verbose = true;
+        pull.rebase = true;
+        push = {
+          default = "simple";
+          autoSetupRemote = true;
+          followTags = true;
+        };
+        merge.conflictstyle = "zdiff3";
+        column.ui = "auto";
+        branch.sort = "-comitterdate";
+        tag.sort = "version:refname";
+        init.defaultBranch = "main";
+        fetch = {
+          prune = true;
+          pruneTags = true;
+          all = true;
+        };
+        help.autocorrect = "prompt";
+        rerere = {
+          enabled = true;
+          autoupdate = true;
+        };
+        core.excludesfile = "~/.gitignore";
+        rebase = {
+          autoSquash = true;
+          autoStash = true;
+          updateRefs = true;
+          fsmonitor = true;
+        };
+        # @TODO: Remove user email, add GPG key shared between devices?
+        # user = {
+        #   email = "github@colorman.me";
+        #   name = "TheColorman";
+        #   signingKey = "AB110475B417291D"; # @TODO: Can this key be created dynamically?
+        # };
+        # commit = {
+        #   gpgsign = true;
+        # };
       };
     };
+    gh.enable = true;
   };
 }
