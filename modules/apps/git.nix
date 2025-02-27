dd8bf23c3196a9c21d6e058cde8a403a7148cca9
TheColorman <github@colorman.me>
2025-02-27 23:25:10 +0100

    Add more git aliases

--- modules/apps/git.nix
+++ modules/apps/git.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }:
+{ config, ... }:
 let
   user = config.my.username;
 in {
@@ -15,6 +15,12 @@ in {
         c = "commit";
         co = "checkout";
         s = "switch";
+        st = "status";
+        unda = "reset --soft HEAD~1";
+        lg1 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
+        lg2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
+        lg = "lg1";
+        aa = "!git add -A && git status";
       };
       extraConfig = {
         diff = {
