b9c92b062ef9c39177a0dfa18855ec78ec22d1dd
TheColorman <github@colorman.me>
2025-04-03 10:37:44 +0200

    refactor(apps/kitty): increase separation of optional enables

--- modules/apps/kitty.nix
+++ modules/apps/kitty.nix
@@ -3,35 +3,43 @@
   config,
   ...
 }: let
-  tmuxEnabled = config.home-manager.users."${config.my.username}".programs.tmux.enable;
-  zshEnabled = config.home-manager.users."${config.my.username}".programs.zsh.enable;
+  inherit (lib) mkMerge mkIf;
+
+  tmuxEnabled = config.home-manager.users.${config.my.username}.programs.tmux.enable;
+  zshEnabled = config.home-manager.users.${config.my.username}.programs.zsh.enable;
 in {
-  home-manager.users."${config.my.username}" = {
-    programs.kitty = {
-      enable = true;
-      # font.name = "CaskaydiaCove Nerd Font"; # Now managed by stylix
-      settings = {
-        scrollback_lines = 10000;
-        enable_audio_bell = false;
-        confirm_os_window_close = 0;
-      };
-      keybindings = {
-        "ctrl+c" = "copy_or_interrupt";
-      };
-      shellIntegration.enableZshIntegration = zshEnabled;
+  config = mkMerge [
+    {
+      home-manager.users."${config.my.username}" = {
+        programs.kitty = {
+          enable = true;
+          settings = {
+            # font.name = "CaskaydiaCove Nerd Font"; # Now managed by stylix
+            scrollback_lines = 10000;
+            enable_audio_bell = false;
+            confirm_os_window_close = 0;
+            background_blur = 1;
+            hide_window_decorations = true;
+            cursor_trail = 3;
+          };
 
-      settings = {
-        startup_session = lib.optionalString tmuxEnabled "launch.conf";
-        background_blur = 1;
-        hide_window_decorations = true;
-        cursor_trail = 3;
-      };
-    };
+          keybindings = {
+            "ctrl+c" = "copy_or_interrupt";
+          };
 
-    xdg.configFile = lib.mkIf tmuxEnabled {
-      "kitty/launch.conf".text = ''
-        launch sh -c "tmux new -t main" -2
-      '';
-    };
-  };
+          shellIntegration.enableZshIntegration = zshEnabled;
+        };
+      };
+    }
+
+    (mkIf tmuxEnabled {
+      # Auto start tmux in kitty, with new sessions that share windows
+      xdg.configFile = {
+        "kitty/launch.conf".text = ''
+          launch sh -c "tmux new -t main" -2
+        '';
+      };
+      home-manager.users.${config.my.username}.programs.kitty.settings.startup_session = "launch.conf";
+    })
+  ];
 }
