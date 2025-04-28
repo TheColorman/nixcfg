2d704bfd95894561ed97646dde58e59841c54458
TheColorman <github@colorman.me>
2025-04-29 00:29:36 +0200

    feat(kanata): add numpad and qwerty layers

--- modules/apps/kanata.nix
+++ modules/apps/kanata.nix
@@ -1,4 +1,4 @@
-{...}: {
+{
   services.kanata = {
     enable = true;
     keyboards.default = {
@@ -10,9 +10,11 @@
           grv 1  2  3  4  5  6  7  8  9  0
           tab  q  w  e  r  t  y  u  i  o  p
           caps  a  s  d  f  g  h  j  k  l  ;
-          lsft   z  x  c  v  b  n  m  ,  .
-                             spc  ⎇›
+          lsft   z  x  c  v  b  n  m  ,  .  ⁄
+          lctl     ‹⎇        spc        ⎇›
         )
+        ;; I use symbols cause they're not as wide as the text versions.
+        ;; See https://github.com/jtroo/kanata/blob/main/docs/fancy_symbols.md
 
         (defvar
           ;; Currently not used
@@ -30,22 +32,47 @@
 
         (defalias
           ext (layer-while-held navigation)
+          deflt (layer-switch default-layer)
+          ralt (one-shot 500 (layer-while-held right-alt))
+          qwerty (layer-switch qwerty)
+          numpad (layer-switch numpad)
+
         )
         (deflayer (default-layer)
           grv 1   2   3   4   5   6   7   8   9   0
           tab  q   w   e   r   t   y   u   i   o   p
           @ext  a   s   d   f   g   h   j   k   l   ;
-          lsft   z   x   c   v   b   n   m   ,   .
-                                 spc  ⎇›
+          lsft   z   x   c   v   b   n   m   ,   .   ⁄
+          lctl       ‹⎇           spc          @ralt
         )
-        ;; I use symbols cause they're not as wide as the text versions.
-        ;; See https://github.com/jtroo/kanata/blob/main/docs/fancy_symbols.md
+        (deflayer (right-alt)
+        	_   _   _   _   _   _   _   _   _   _   _
+        	_    _   _   _   _   _   _   _   _   _   _
+        	@ext  _   _   _   _   _   _   _   _   _   _
+        	_      _   _   _   _   _   _   _   _   _   _
+        	@qwerty    _             _           @ralt
+        )
         (deflayer (navigation)
           grv _   _   _   _   _   _   _   _   _   _
           tab  ⎋   _   _   _   _   _   ⇤   ⇞   ⇟   ⇥
           @ext  ‹⎇  ‹❖  ‹⇧  ‹⌃  _   _   ◀   ▲   ▼   ▶
-          lsft  C-z C-x  _  C-v C-b  _   ⌫   ⌦   _
-                                enter  ⎇›
+          lsft  C-x C-c C-d C-v C-z  _   ⌫   ⌦   _   _
+          lctl    @numpad    enter               ⎇›
+        )
+        ;; Since OS already uses colemak-dh, this is essentially a reverse mapping back to qwerty
+        (deflayer (qwerty)
+          grv 1  2  3  4  5  6  7  8  9  0
+          tab  q  w  k  s  f  o  i  l  ;  r
+          caps  a  d  c  e  g  m  y  n  u  p
+          lsft   b  z  x  v  t  j  h  ,  .  ⁄
+          lctl     ‹⎇        spc     @deflt
+        )
+        (deflayer (numpad)
+          grv _   _   _   _   _   _   _   _   kp/ kp*
+          tab  _   _   _   _   _   _   7   8   9   kp-
+          @ext  _   _   _   _   _   _   4   5   6   kp+
+          lsft   _   _   _   _   _   0   1   2   3   kp.
+          lctl     @deflt  NumpadEnter            _
         )
       '';
       extraDefCfg = ''
