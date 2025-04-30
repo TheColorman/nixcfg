08d0c11b2ace58f2832a5e8003a91f6cf6a6e9e9
TheColorman <github@colorman.me>
2025-04-30 12:51:47 +0200

    feat(kanata): make ralt multi-functional
    allows me to use right alt both as a layer (as part of the combination
    to switch to QWERTY), as well as still using its default behavior of
    typing alternative versions of keys, most importantly æ, ø and å,
    along with their shifted variants

--- modules/apps/kanata.nix
+++ modules/apps/kanata.nix
@@ -3,9 +3,11 @@
     enable = true;
     keyboards.default = {
       config = ''
-        ;; Inspired by https://www.youtube.com/watch?v=sLWQ4Gx88h4 and https://github.com/mhantsch/maxtend/blob/main/kanata/colemax-maxtend.kbd
+        ;; Inspired by https://www.youtube.com/watch?v=sLWQ4Gx88h4 and
+        ;; https://github.com/mhantsch/maxtend/blob/main/kanata/colemax-maxtend.kbd
 
-        ;; Even though I use Colemak as my OS layout, this still has to be defined as qwerty
+        ;; Even though I use Colemak as my OS layout, this still has to be
+        ;; defined as qwerty
         (defsrc
           grv 1  2  3  4  5  6  7  8  9  0
           tab  q  w  e  r  t  y  u  i  o  p
@@ -33,7 +35,8 @@
         (defalias
           ext (layer-while-held navigation)
           deflt (layer-switch default-layer)
-          ralt (one-shot 500 (layer-while-held right-alt))
+          ralt (multi (one-shot 500 (layer-while-held right-alt))
+                                    (one-shot 500 ralt))
           qwerty (layer-switch qwerty)
           numpad (layer-switch numpad)
 
@@ -59,7 +62,8 @@
           lsft  C-x C-c C-d C-v C-z  _   ⌫   ⌦   _   _
           lctl    @numpad    enter               ⎇›
         )
-        ;; Since OS already uses colemak-dh, this is essentially a reverse mapping back to qwerty
+        ;; Since OS already uses colemak-dh, this is essentially a reverse
+        ;; mapping back to qwerty
         (deflayer (qwerty)
           grv 1  2  3  4  5  6  7  8  9  0
           tab  q  w  k  s  f  o  i  l  ;  r
