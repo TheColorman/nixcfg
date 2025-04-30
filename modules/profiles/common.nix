9a8952ff110e1860aaec1a1eb1456231140ce752
TheColorman <github@colorman.me>
2025-04-30 17:22:05 +0200

    feat(nix): add system-wide nix flake registries

--- modules/profiles/common.nix
+++ modules/profiles/common.nix
@@ -19,10 +19,29 @@ in {
   };
 
   config = {
-    # flakification
-    nix.settings = {
-      experimental-features = ["nix-command" "flakes"];
-      trusted-users = ["root" username];
+    nix = {
+      settings = {
+        # flakes
+        experimental-features = ["nix-command" "flakes"];
+        # allow main user to trust binary caches among other things
+        trusted-users = ["root" username];
+      };
+
+      # A flake registry is a repository that contains a nix flake. This
+      # attrset specifies system-wide registries and their aliases. For
+      # example, the nixpkgs alias, which by default points to
+      # github:nixos/nixpkgs/master, is here set to instead point to the
+      # nixpkgs revision used in this system flake. This prevents the system
+      # from trying to fetch the newest nixpkgs revision every time I try do
+      # `nix run nixpkgs#` or `nix shell nixpkgs#`.
+      registry = {
+        nixpkgs.flake = inputs.nixpkgs;
+        nur.to = {
+          owner = "nix-community";
+          repo = "nur";
+          type = "github";
+        };
+      };
     };
 
     # Allow execution of dynamic binaries
