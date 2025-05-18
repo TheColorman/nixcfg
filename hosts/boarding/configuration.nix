66f6ba81c36b5c44e9cea4df6647414e3d3fff66
Colorman <foss@colorman.me>
2025-05-18 13:18:52 +0200

    feat(boarding): add gnome-keyring

--- hosts/boarding/configuration.nix
+++ hosts/boarding/configuration.nix
@@ -48,6 +48,8 @@ in {
     docker = "/run/current-system/sw/bin/docker";
   };
 
+  services.gnome.gnome-keyring.enable = true;
+
   users.users."${username}" = {
     isNormalUser = true;
     hashedPassword = "$y$j9T$sD7TNPmhg9Zxso6eUqMV9/$Wa/zdt3yOxWfrY3aLlRjbajyqy/6r6oYInvjplj02O9";
