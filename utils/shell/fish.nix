8e19b390fec6f5e2c522bf47a5bd07b4f381a0fc
Colorman <foss@colorman.me>
2025-05-24 12:32:37 +0200

    feat(fish): create preInit option

--- utils/shell/fish.nix
+++ utils/shell/fish.nix
@@ -1,75 +1,92 @@
 {
   pkgs,
   config,
+  lib,
   ...
 }: let
+  inherit (lib.options) mkOption;
+  inherit (lib) types;
   inherit (config.my) username;
   inherit (pkgs) fish;
+  cfg = config.my.fish;
 in {
   imports = [
     ./default.nix
     ./starship.nix
   ];
 
-  programs.fish.enable = true;
-  users.users."${username}".shell = fish;
+  options.my.fish.preInit = mkOption {
+    type = types.lines;
+    default = "";
+    description = ''
+      Shell script code called before any plugins are initialised
+    '';
+  };
 
-  home-manager.users."${username}" = {
-    home.shell.enableFishIntegration = true;
-    programs.fish = {
-      enable = true;
-      functions = {
-        pythonEnv = ''
-          function pythonEnv --description 'start a nix-shell with the given python packages' --argument pythonVersion
-            if set -q argv[2]
-              set argv $argv[2..-1]
+  config = {
+    my.fish.preInit = ''
+      # Refresh async shell when PWD changes
+      set -g -a async_prompt_on_variable PWD
+    '';
+
+    programs.fish.enable = true;
+    users.users."${username}".shell = fish;
+
+    home-manager.users."${username}" = {
+      home.shell.enableFishIntegration = true;
+      programs.fish = {
+        enable = true;
+        functions = {
+          pythonEnv = ''
+            function pythonEnv --description 'start a nix-shell with the given python packages' --argument pythonVersion
+              if set -q argv[2]
+                set argv $argv[2..-1]
+              end
+
+              for el in $argv
+                set ppkgs $ppkgs "python"$pythonVersion"Packages.$el"
+              end
+
+              nix-shell -p $ppkgs
             end
-
-            for el in $argv
-              set ppkgs $ppkgs "python"$pythonVersion"Packages.$el"
-            end
-
-            nix-shell -p $ppkgs
+          '';
+        };
+        interactiveShellInit = ''
+          set -U fish_greeting
+        '';
+        shellInitLast = ''
+          # disable transient prompt on C-c
+          # instead use custom function that just clears the command line
+          function __transient_ctrl_c_execute
+            commandline ""
           end
         '';
+        preferAbbrs = true;
+        plugins = [
+          {
+            name = "fish-async-prompt";
+            src = pkgs.fetchFromGitHub {
+              owner = "infused-kim";
+              repo = "fish-async-prompt";
+              rev = "07e107635e693734652b0709dd34166820f1e6ff";
+              hash = "sha256-rE80IuJEqnqCIE93IzeT2Nder9j4fnhFEKx58HJUTPk=";
+            };
+          }
+          {
+            name = "transient";
+            src = pkgs.fetchFromGitHub {
+              owner = "zzhaolei";
+              repo = "transient.fish";
+              rev = "7091a1ef574e4c2d16779e59d37ceb567128c787";
+              hash = "sha256-rZqMQiVGEEYus5MxkpFhaXnjVStmsjWkGly4B6bjcks=";
+            };
+          }
+          {
+            name = "000-preinit";
+            src = pkgs.writeTextDir "conf.d/000-preinit.fish" cfg.preInit;
+          }
+        ];
       };
-      interactiveShellInit = ''
-        set -U fish_greeting
-      '';
-      shellInitLast = ''
-        # disable transient prompt on C-c
-        # instead use custom function that just clears the command line
-        function __transient_ctrl_c_execute
-          commandline ""
-        end
-      '';
-      preferAbbrs = true;
-      plugins = [
-        {
-          name = "fish-async-prompt";
-          src = pkgs.fetchFromGitHub {
-            owner = "infused-kim";
-            repo = "fish-async-prompt";
-            rev = "07e107635e693734652b0709dd34166820f1e6ff";
-            hash = "sha256-rE80IuJEqnqCIE93IzeT2Nder9j4fnhFEKx58HJUTPk=";
-          };
-        }
-        {
-          name = "transient";
-          src = pkgs.fetchFromGitHub {
-            owner = "zzhaolei";
-            repo = "transient.fish";
-            rev = "7091a1ef574e4c2d16779e59d37ceb567128c787";
-            hash = "sha256-rZqMQiVGEEYus5MxkpFhaXnjVStmsjWkGly4B6bjcks=";
-          };
-        }
-        {
-          name = "000-preinit";
-          src = pkgs.writeTextDir "conf.d/000-preinit.fish" ''
-            set -g -a async_prompt_on_variable PWD
-          '';
-        }
-      ];
     };
   };
 }
