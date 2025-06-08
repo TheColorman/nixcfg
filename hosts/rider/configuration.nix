555a5cdb50a0d853e26bd3a6d61a008c65f05ef9
Colorman <foss@colorman.me>
2025-06-08 15:56:32 +0200

    feat(rider): finish initial configuration

--- hosts/rider/configuration.nix
+++ hosts/rider/configuration.nix
@@ -1,9 +1,10 @@
 {
   pkgs,
   outputs,
+  config,
   ...
 }: let
-  user = "color";
+  username = "color";
 in {
   imports = with outputs.modules; [
     ./hardware-configuration.nix
@@ -13,25 +14,64 @@ in {
     apps-jujutsu
     apps-neovim
     apps-nix
+    services-gpg
+    services-sops
+    services-tailscale
     system-networking
     utils-shell
     utils-shell-fish
+    utils-nas_mounts
   ];
 
-  environment.systemPackages = with pkgs; [vim];
+  my = {
+    inherit username;
+    stateVersion = "25.05";
+  };
 
+  environment.systemPackages = with pkgs; [vim];
   services.openssh.enable = true;
 
-  my.stateVersion = "25.05";
+  users.users."${username}" = {
+    isNormalUser = true;
+    hashedPasswordFile = config.sops.secrets.color_passwd.path;
+    extraGroups = ["wheel"];
+    packages = with pkgs; [
+      fastfetch
+      unzip
+      p7zip
+      aria2
+      killall
+      dig
+      ripgrep
+      cachix
+      jq
+    ];
+  };
 
-  users = {
-    mutableUsers = false;
-    users."${user}" = {
-      isNormalUser = true;
-      hashedPassword = "$y$j9T$Vl8jrClizkWjKwA/x42xH1$nYnTieowMq7SnXB.Qfibf8IFs6iuYkozM5myB.WjPb5";
-      extraGroups = ["wheel"];
+  hardware.bluetooth.enable = true;
+
+  time.timeZone = "Europe/Copenhagen";
+  i18n = {
+    defaultLocale = "en_DK.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "da_DK.UTF-8";
+      LC_IDENTIFICATION = "da_DK.UTF-8";
+      LC_MEASUREMENT = "da_DK.UTF-8";
+      LC_MONETARY = "da_DK.UTF-8";
+      LC_NAME = "da_DK.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "da_DK.UTF-8";
+      LC_TELEPHONE = "da_DK.UTF-8";
+      LC_TIME = "da_DK.UTF-8";
     };
   };
+
+  # networking.interfaces."end0".ipv4.addresses = [
+  #   {
+  #     address = "192.168.50.58";
+  #     prefixLength = 24;
+  #   }
+  # ];
 
   hardware.enableRedistributableFirmware = true;
 
