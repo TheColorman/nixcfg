3b889356d644f44e4c3363c4ee73a92173dc503e
Colorman <foss@colorman.me>
2025-06-17 15:56:13 +0200

    feat(services): add sonarr

---
+++ services/sonarr.nix
@@ -0,0 +1,25 @@
+{
+  outputs,
+  config,
+  ...
+}: {
+  imports = [
+    outputs.modules.services-sops
+  ];
+
+  services.sonarr = {
+    enable = true;
+    environmentFiles = [config.sops.templates."sonarr.env".path];
+  };
+
+  sops = {
+    secrets."services/sonarr/apiKey".restartUnits = ["sonarr.service"];
+
+    templates."sonarr.env" = {
+      content = ''
+        SONARR__AUTH__APIKEY=${config.sops.placeholder."services/sonarr/apiKey"}
+      '';
+      restartUnits = ["sonarr.service"];
+    };
+  };
+}
