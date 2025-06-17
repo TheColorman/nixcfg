e980420494126e8b6b5e8e30f03d0a5c5ffef72c
Colorman <foss@colorman.me>
2025-06-17 18:50:44 +0200

    feat(services): add nextcloud

---
+++ services/nextcloud.nix
@@ -0,0 +1,72 @@
+{
+  inputs,
+  outputs,
+  pkgs,
+  config,
+  ...
+}: let
+  evalSecrets = (import "${inputs.nix-secrets}/evaluation-secrets.nix").services.nextcloud;
+in {
+  imports = [outputs.modules.services-cloudflared];
+
+  services = {
+    nextcloud = {
+      enable = true;
+      # Pinned
+      package = pkgs.nextcloud31;
+      webfinger = true;
+      notify_push = {
+        enable = true;
+        bendDomainToLocalhost = true;
+      };
+      maxUploadSize = "16G";
+      https = true;
+      hostName = evalSecrets.hostname;
+      extraApps = {
+        inherit
+          (pkgs.nextcloud31Packages.apps)
+          app_api
+          end_to_end_encryption
+          notify_push
+          ;
+      };
+      config = {
+        dbtype = "pgsql";
+        adminuser = "admin";
+        adminpassFile = config.sops.secrets."services/nextcloud/adminpass".path;
+      };
+
+      database.createLocally = true;
+
+      secretFile = config.sops.templates."nextcloud.settings.json".path;
+    };
+
+    nginx.virtualHosts."${evalSecrets.hostname}".listen = [
+      {
+        addr = "127.0.0.1";
+        port = 52531;
+      }
+    ];
+  };
+
+  my.cloudflared.tunnels.nextcloud.tokenFile = config.sops.secrets."services/nextcloud/tunnel_token".path;
+
+  # Sops key setup
+  sops = {
+    secrets = {
+      "services/nextcloud/adminpass" = {};
+      "services/nextcloud/tunnel_token" = {};
+      "services/nextcloud/instanceid" = {};
+      "services/nextcloud/passwordsalt" = {};
+      "services/nextcloud/secret" = {};
+    };
+    templates."nextcloud.settings.json".content = let
+      instanceid = config.sops.placeholder."services/nextcloud/instanceid";
+      passwordsalt = config.sops.placeholder."services/nextcloud/passwordsalt";
+      secret = config.sops.placeholder."services/nextcloud/secret";
+    in
+      builtins.toJSON {
+        inherit instanceid passwordsalt secret;
+      };
+  };
+}
