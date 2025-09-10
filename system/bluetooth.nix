9b793a118a4463642a3e7f94565858bd7c315f86
Colorman <foss@colorman.me>
2025-09-10 18:22:33 +0200

    feat(bluetooth): allow HID in Userspace

--- system/bluetooth.nix
+++ system/bluetooth.nix
@@ -2,6 +2,12 @@
   # hyprland does not have a bluetooth manager built in
   missingBTManager = config.programs.hyprland.enable;
 in {
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    # Required for DualShock 4 BT.
+    # See https://wiki.gentoo.org/wiki/Sony_DualShock
+    input.General.UserspaceHID = true;
+  };
+
   services.blueman.enable = missingBTManager;
 }
