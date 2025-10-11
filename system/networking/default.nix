d3844bf46ef98745e4123a95cf7877d9645ddbec
Colorman <foss@colorman.me>
2025-10-11 20:14:28 +0200

    del(networking): remove hardcoded ip addresses

--- system/networking/default.nix
+++ system/networking/default.nix
@@ -17,13 +17,7 @@
       hostName = systemName;
       networkmanager.enable = true;
       networkmanager.wifi.scanRandMacAddress = false;
-      hosts = let
-        base = {"10.42.0.4" = ["pwnagotchi.local"];};
-        tailscale = lib.optionalAttrs config.services.tailscale.enable {"192.168.50.222" = ["truenas"];};
-        full = base // tailscale;
-      in
-        full;
-      nameservers = ["192.168.50.58" "1.1.1.1"];
+      nameservers = ["1.1.1.1"];
 
       # bitches trying to deauth me n shit
       wireless.extraConfig = ''
