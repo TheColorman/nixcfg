eea32c13a46e4393e7fe7dfd4e6e4940e18c6825
Colorman <foss@colorman.me>
2025-11-22 00:32:10 +0100

    Switch to stable
    build(inputs): update 25.05 -> 25.11

--- flake.nix
+++ flake.nix
@@ -60,17 +60,17 @@
 
   inputs = {
     # == Primary modules ==
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # == Add-ons ==
     stylix = {
       # Theming
-      url = "github:nix-community/stylix/master";
+      url = "github:nix-community/stylix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # Secrets
