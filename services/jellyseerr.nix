bf1d7744168bfe959964daa8e261fe2fcbd833da
Colorman <foss@colorman.me>
2025-11-28 23:49:15 +0100

    feat(jellyseerr): add cloudflared tunnel

--- services/jellyseerr.nix
+++ services/jellyseerr.nix
@@ -1,6 +1,16 @@
 {
+  outputs,
+  config,
+  ...
+}: {
+  imports = [outputs.modules.services-cloudflared];
+
   services.jellyseerr = {
     enable = true;
     openFirewall = true;
   };
+
+  my.cloudflared.tunnels.jellyseerr.tokenFile = config.sops.secrets."services/jellyseerr/tunnel_token".path;
+
+  sops.secrets."services/jellyseerr/tunnel_token" = {};
 }
