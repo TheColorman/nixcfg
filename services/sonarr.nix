08e265d871990c71caf45535ecd0f2ce1991c45b
Colorman <foss@colorman.me>
2025-11-29 20:24:08 +0100

    feat(sonarr): add self-signed TLS certificate

--- services/sonarr.nix
+++ services/sonarr.nix
@@ -2,17 +2,35 @@
   outputs,
   config,
   ...
-}: {
+}: let
+  domain = "sonarr.color";
+
+  cfg = config.services.sonarr;
+  crtCfg = config.my.certificates.certs."${domain}";
+in {
   imports = [
     outputs.modules.services-sops
   ];
+  services = {
+    sonarr = {
+      enable = true;
+      environmentFiles = [config.sops.templates."sonarr.env".path];
 
-  services.sonarr = {
-    enable = true;
-    environmentFiles = [config.sops.templates."sonarr.env".path];
+      openFirewall = true;
+    };
 
-    openFirewall = true;
+    nginx.virtualHosts."${domain}" = {
+      locations."/".proxyPass = "http://127.0.0.1:${toString cfg.settings.server.port}";
+      forceSSL = true;
+
+      sslCertificateKey = crtCfg.key.path;
+      sslCertificate = crtCfg.crt.path;
+    };
   };
+
+  users.users."nginx".extraGroups = ["certboy"];
+
+  my.certificates.certs."${domain}" = {};
 
   sops = {
     secrets."services/sonarr/apiKey".restartUnits = ["sonarr.service"];
