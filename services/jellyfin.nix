86ab366673c83be6f990dfe0d0dced9e5b6b8eed
Colorman <foss@colorman.me>
2025-12-14 01:04:01 +0100

    feat(jellyfin): add local domain

--- services/jellyfin.nix
+++ services/jellyfin.nix
@@ -2,24 +2,39 @@
   outputs,
   config,
   ...
-}: {
+}: let
+  domain = "jellyfin.color";
+
+  crtCfg = config.my.certificates.certs."${domain}";
+in {
   imports = [
     outputs.modules.services-cloudflared
   ];
+  services = {
+    # https://jellyfin.org/docs/general/post-install/networking/
+    # Port  Protocol  Configurable  Description
+    # 8096  TCP        ✔️           Default HTTP
+    # 8920  TCP        ✔️           Default HTTPS
+    # 7359  UDP        ❌           Client Discovery
+    jellyfin.enable = true;
 
-  # https://jellyfin.org/docs/general/post-install/networking/
-  # Port  Protocol  Configurable  Description
-  # 8096  TCP        ✔️           Default HTTP
-  # 8920  TCP        ✔️           Default HTTPS
-  # 7359  UDP        ❌           Client Discovery
-  services.jellyfin.enable = true;
+    nginx.virtualHosts."${domain}" = {
+      locations."/".proxyPass = "http://127.0.0.1:30013";
+      forceSSL = true;
+
+      sslCertificateKey = crtCfg.key.path;
+      sslCertificate = crtCfg.crt.path;
+    };
+  };
 
   networking.firewall = {
     allowedTCPPorts = [30013];
     allowedUDPPorts = [30013];
   };
-
-  my.cloudflared.tunnels.jellyfin.tokenFile = config.sops.secrets."services/jellyfin/tunnel_token".path;
+  my = {
+    certificates.certs."${domain}" = {};
+    cloudflared.tunnels.jellyfin.tokenFile = config.sops.secrets."services/jellyfin/tunnel_token".path;
+  };
 
   sops.secrets."services/jellyfin/tunnel_token" = {};
 }
