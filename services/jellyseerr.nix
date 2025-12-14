b6e475c9600af9266b7f21904cf026f3528e352f
Colorman <foss@colorman.me>
2025-12-14 01:04:01 +0100

    feat(jellyseerr): add local domain

--- services/jellyseerr.nix
+++ services/jellyseerr.nix
@@ -2,15 +2,31 @@
   outputs,
   config,
   ...
-}: {
+}: let
+  domain = "jellyseerr.color";
+
+  cfg = config.services.jellyseerr;
+  crtCfg = config.my.certificates.certs."${domain}";
+in {
   imports = [outputs.modules.services-cloudflared];
+  services = {
+    jellyseerr = {
+      enable = true;
+      openFirewall = true;
+    };
 
-  services.jellyseerr = {
-    enable = true;
-    openFirewall = true;
+    nginx.virtualHosts."${domain}" = {
+      locations."/".proxyPass = "http://127.0.0.1:${toString cfg.port}";
+      forceSSL = true;
+
+      sslCertificateKey = crtCfg.key.path;
+      sslCertificate = crtCfg.crt.path;
+    };
   };
-
-  my.cloudflared.tunnels.jellyseerr.tokenFile = config.sops.secrets."services/jellyseerr/tunnel_token".path;
+  my = {
+    certificates.certs."${domain}" = {};
+    cloudflared.tunnels.jellyseerr.tokenFile = config.sops.secrets."services/jellyseerr/tunnel_token".path;
+  };
 
   sops.secrets."services/jellyseerr/tunnel_token" = {};
 }
