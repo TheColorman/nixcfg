3830fb61b2630792cf228ee70616df81bfce9a68
Colorman <foss@colorman.me>
2025-12-14 13:45:35 +0100

    feat(anchorr): fill in more configuration

--- services/anchorr.nix
+++ services/anchorr.nix
@@ -3,7 +3,9 @@
   inputs,
   config,
   ...
-}: {
+}: let
+  evalSecrets = (import "${inputs.nix-secrets}/evaluation-secrets.nix").services.anchorr;
+in {
   imports = [
     inputs.nur.modules.nixos.default
     inputs.nur.legacyPackages.${systemPlatform}.repos.colorman.modules.anchorr
@@ -22,6 +24,13 @@
       WEBHOOK_PORT = "8282";
       AUTO_START_BOT = "true";
       NOTIFY_ON_AVAILABLE = "false";
+      JELLYFIN_NOTIFICATION_LIBRARIES = {
+        "0c41907140d802bb58430fed7e2cd79e" = "";
+        "f137a2dd21bbc1b99aa5c0f6bf02a805" = "";
+        "4514ec850e5ad0c47b58444e17b6346c" = "";
+        "34f331a89ce405e2b877d68d5ee4d4a2" = "";
+      };
+      USER_MAPPINGS = evalSecrets.userMappings;
     };
     settingsFile = config.sops.templates."anchorr.config.json".path;
   };
@@ -39,6 +48,9 @@
       "services/anchorr/jellyfinApiKey" = {};
       "services/jellyfin/serverId" = {};
       "services/anchorr/jellyfinChannelId" = {};
+      "services/anchorr/jwtSecret" = {};
+      "services/anchorr/adminHashedPassword" = {};
+      "services/anchorr/allowedRole" = {};
     };
 
     templates."anchorr.config.json".content = builtins.toJSON {
@@ -51,6 +63,18 @@
       JELLYFIN_API_KEY = config.sops.placeholder."services/anchorr/jellyfinApiKey";
       JELLYFIN_SERVER_ID = config.sops.placeholder."services/jellyfin/serverId";
       JELLYFIN_CHANNEL_ID = config.sops.placeholder."services/anchorr/jellyfinChannelId";
+      JWT_SECRET = config.sops.placeholder."services/anchorr/jwtSecret";
+      USERS = [
+        {
+          id = "1765715242454";
+          username = "colorman";
+          password = config.sops.placeholder."services/anchorr/adminHashedPassword";
+          createdAt = "2025-12-14T12:27:22.454Z";
+        }
+      ];
+      ROLE_ALLOWLIST = [
+        config.sops.placeholder."services/anchorr/allowedRole"
+      ];
     };
   };
 }
