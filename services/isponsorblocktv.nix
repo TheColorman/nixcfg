cb591ec5d6ed175819572e48bcba101147f06374
Colorman <foss@colorman.me>
2025-12-15 20:36:51 +0100

    feat(services): add isponsorblocktv

---
+++ services/isponsorblocktv.nix
@@ -0,0 +1,9 @@
+{
+  virtualisation.oci-containers.containers.isponsorblocktv = {
+    image = "ghcr.io/dmunozv04/isponsorblocktv:latest";
+    volumes = [
+      "/var/lib/isponsorblocktv:/app/data"
+    ];
+    extraOptions = ["--network=host"];
+  };
+}
