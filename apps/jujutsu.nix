0c63fd2f60ba361dcf84998033199ae8a8826a15
Colorman <foss@colorman.me>
2026-01-18 00:21:20 +0100

    fix(jujutsu): also use EDITOR environment variable from environment

--- apps/jujutsu.nix
+++ apps/jujutsu.nix
@@ -12,6 +12,7 @@
 
   editor =
     config.home-manager.users."${username}".home.sessionVariables.EDITOR
+    or config.environment.variables.EDITOR
     or "";
 in {
   home-manager.users.${username} = {
