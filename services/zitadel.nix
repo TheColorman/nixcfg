23f845c3fd033c9dd1938322450ab11532f6f0c2
Colorman <foss@colorman.me>
2026-01-29 20:37:39 +0100

    fix(services/zitadel): fix issues with database init

--- services/zitadel.nix
+++ services/zitadel.nix
@@ -3,8 +3,10 @@
   config,
   outputs,
   lib,
+  pkgs,
   ...
 }: let
+  settingsFormat = pkgs.formats.yaml {};
   evalSecrets =
     (import "${inputs.nix-secrets}/evaluation-secrets.nix").services.zitadel;
 
@@ -26,6 +28,7 @@ in {
       '';
     }
   ];
+
   services = {
     zitadel = {
       enable = true;
@@ -38,11 +41,16 @@ in {
         ExternalSecure = true;
         TLS.Enabled = false;
         Database.postgres = {
-          Host = "localhost";
+          Host = "/run/postgresql";
           Port = 5432;
           Database = "zitadel";
           User = {
             Username = "zitadel";
+            SSL.Mode = "disable";
+          };
+          Admin = {
+            Username = "zitadel";
+            ExistingDatabase = "zitadel";
             SSL.Mode = "disable";
           };
         };
@@ -61,6 +69,31 @@ in {
       ];
     };
   };
+
+  # This is copied from the source of the zitadel nixpkgs service, with the
+  # original "zitadel start-from-init" command replaced with
+  # "zitadel start-from-setup", as "start-from-init" always tries to use admin
+  # login to create the Postgres database and role, whereas "start-from-setup"
+  # supports having the database and role already created (which is done by the
+  # postgresql service).
+  # NOTE: If doing this, you also need to manually run all the init SQL against
+  # the database, ensuring that all schemas, tables and functions are created
+  # AS the zitadel user. The commands required to run are located at
+  # https://github.com/zitadel/zitadel/blob/f7277b8641af5849d77db0440f0f7c91f98db2bd/cmd/initialise/sql/README.md
+  systemd.services.zitadel.script = let
+    configFile = settingsFormat.generate "config.yaml" cfg.settings;
+    stepsFile = settingsFormat.generate "steps.yaml" cfg.steps;
+
+    args = lib.cli.toCommandLineShellGNU {} {
+      config = cfg.extraSettingsPaths ++ [configFile];
+      steps = cfg.extraStepsPaths ++ [stepsFile];
+      masterkeyFile = cfg.masterKeyFile;
+      inherit (cfg) tlsMode;
+    };
+  in
+    lib.mkForce ''
+      zitadel start-from-setup ${args}
+    '';
 
   my.cloudflared.tunnels.zitadel.tokenFile =
     config.sops.secrets."services/zitadel/tunnelToken".path;
