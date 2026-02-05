ebc6a2bfab66e86a867eb8e03c22e36443ce53c2
Colorman <foss@colorman.me>
2026-02-06 02:13:29 +0700

    feat(system/desktop/hyprland): add uwsm helper

--- system/desktop/hyprland/default.nix
+++ system/desktop/hyprland/default.nix
@@ -1,6 +1,17 @@
-{
+{pkgs, ...}: {
   imports = [
     ./hyprland.nix
     ./hyprcursor.nix
+  ];
+
+  environment.systemPackages = [
+    (pkgs.writeShellApplication {
+      name = "hypr";
+      text = ''
+        if uwsm check may-start; then
+          uwsm start default
+        fi
+      '';
+    })
   ];
 }
