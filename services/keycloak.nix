98dfaf8d1aa8278014e017ea6b35ef40b5ea880f
Colorman <foss@colorman.me>
2026-02-08 00:24:24 +0700

    feat(services/keycloak): ensure database user exists

--- services/keycloak.nix
+++ services/keycloak.nix
@@ -6,22 +6,41 @@
 }: let
   evalSecrets =
     (import "${inputs.nix-secrets}/evaluation-secrets.nix").services.keycloak;
+
+  cfg = config.services.keycloak;
 in {
-  services.keycloak = {
-    enable = true;
-    database = {
-      type = "postgresql";
-      host = "/run/postgresql";
+  services = {
+    keycloak = {
+      enable = true;
+      database = {
+        type = "postgresql";
+        host = "/run/postgresql";
+        name = "keycloak";
+        username = cfg.database.name;
+      };
+      plugins = with pkgs; [
+        junixsocket-common.passthru.jar
+        junixsocket-native-common.passthru.jar
+      ];
+      settings = {
+        hostname = "https://${evalSecrets.domain}";
+        # https://developers.cloudflare.com/fundamentals/reference/http-headers
+        proxy-headers = "xforwarded";
+        http-enabled = true;
+        http-port = 8080;
+      };
     };
-    plugins = with pkgs; [
-      junixsocket-common.passthru.jar
-      junixsocket-native-common.passthru.jar
-    ];
-    settings = {
-      hostname = evalSecrets.domain;
-      # https://developers.cloudflare.com/fundamentals/reference/http-headers
-      proxy-headers = "xforwarded";
-      http-enabled = true;
+
+    postgresql = {
+      enable = true;
+      ensureDatabases = [cfg.database.name];
+      ensureUsers = [
+        {
+          name = cfg.database.username;
+          ensureDBOwnership = true;
+          ensureClauses.login = true;
+        }
+      ];
     };
   };
 
