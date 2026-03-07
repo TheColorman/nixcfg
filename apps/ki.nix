57ebfdb082f4f089b7ebb33120bfa0127b701aae
Colorman <foss@colorman.me>
2026-03-07 21:09:42 +0100

    feat(apps/ki): add ki configuration

--- apps/ki.nix
+++ apps/ki.nix
@@ -1,10 +1,19 @@
 { inputs, ... }:
 {
   flake.nixosModules.apps-ki =
-    { pkgs, ... }:
+    { pkgs, config, ... }:
+    let
+      inherit (config.my) username;
+    in
     {
       environment.systemPackages = [
         inputs.ki-editor.packages.${pkgs.stdenv.hostPlatform.system}.default
       ];
+
+      home-manager.users."${username}".xdg.configFile."ki/config.json".text = builtins.toJSON {
+        keyboard_layout = "ColemakAnsiDh";
+        languages = { };
+        theme = "Gruvbox Dark Soft";
+      };
     };
 }
