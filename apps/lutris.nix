e97d443e76179655e2c757c9c441f83506ebc25a
Colorman <foss@colorman.me>
2026-05-09 23:28:02 +0200

    fix(apps/lutris): temporarily disable check for 32-bit OpenLDAP

--- apps/lutris.nix
+++ apps/lutris.nix
@@ -5,6 +5,17 @@
       inherit (config.my) username;
     in
     {
+      # TODO: Temporary "fix" for failing OpenLDAP test on i686. This is likely
+      # a bug in the 32-bit version of OpenLDAP, so this is probably dangerous.
+      # See https://github.com/NixOS/nixpkgs/issues/514113
+      nixpkgs.overlays = [
+        (_: prev: {
+          openldap = prev.openldap.overrideAttrs {
+            doCheck = !prev.stdenv.hostPlatform.isi686;
+          };
+        })
+      ];
+
       home-manager.users."${username}".programs.lutris.enable = true;
     };
 }
