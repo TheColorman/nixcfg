b7b61e6efb812bd47b402f61c719678e7dc2fbe9
Colorman <foss@colorman.me>
2026-05-11 10:26:57 +0200

    feat(apps/ki): make default editor

--- apps/ki.nix
+++ apps/ki.nix
@@ -1,14 +1,22 @@
 { inputs, ... }:
 {
   flake.nixosModules.apps-ki =
-    { pkgs, config, ... }:
+    {
+      pkgs,
+      config,
+      lib,
+      ...
+    }:
     let
       inherit (config.my) username;
     in
     {
-      environment.systemPackages = [
-        inputs.ki-editor.packages.${pkgs.stdenv.hostPlatform.system}.default
-      ];
+      environment = {
+        systemPackages = [
+          inputs.ki-editor.packages.${pkgs.stdenv.hostPlatform.system}.default
+        ];
+        variables.EDITOR = lib.mkForce "ki";
+      };
 
       home-manager.users."${username}".xdg.configFile."ki/config.json".text = builtins.toJSON {
         keyboard_layout = "COLEMAK-DH (ANSI)";
