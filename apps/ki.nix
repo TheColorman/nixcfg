8ec8938dfbcd2b9d2fa48487f6ca146738c1839b
Colorman <foss@colorman.me>
2026-05-22 18:36:56 +0200

    feat(apps/ki): re-enable as default editor

--- apps/ki.nix
+++ apps/ki.nix
@@ -15,8 +15,7 @@
         systemPackages = [
           inputs.ki-editor.packages.${pkgs.stdenv.hostPlatform.system}.default
         ];
-        # TODO: re-enable when I figure out how to enable WakaTime in Ki
-        # variables.EDITOR = lib.mkForce "ki";
+        variables.EDITOR = lib.mkForce "ki";
       };
 
       home-manager.users."${username}".xdg.configFile."ki/config.json".text = builtins.toJSON {
