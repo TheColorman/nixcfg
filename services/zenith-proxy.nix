f7e347eb8b0af25e74fd71b119fa2552aa21c3a2
Colorman <foss@colorman.me>
2026-06-26 20:41:01 +0200

    services/zenith-proxy: add secondary proxy

--- services/zenith-proxy.nix
+++ services/zenith-proxy.nix
@@ -1,12 +1,12 @@
 {
   flake.nixosModules.services-zenith-proxy =
     { config, ... }:
-    {
-      virtualisation.oci-containers.containers.zenith-proxy = {
+    let
+      mkZenithInstance = dir: port: {
         image = "ghcr.io/rfresh2/zenithproxy@sha256:045294f15a2253816f3a8bb1f43abf41d6148ae62cd2fe83eb13a5d3c8bf508d";
-        ports = [ "0.0.0.0:25565:25565" ];
+        ports = [ "0.0.0.0:${port}:25565" ];
         volumes = [
-          "/var/lib/zenith-proxy/:/opt/ZenithProxy/"
+          "/var/lib/${dir}/:/opt/ZenithProxy/"
         ];
         environment = {
           ZENITH_MC_VERSION = "1.21.4";
@@ -15,10 +15,22 @@
         };
         environmentFiles = [ config.sops.templates."zenith-proxy.env".path ];
       };
+    in
+    {
+      virtualisation.oci-containers.containers = {
+        zenith-proxy = mkZenithInstance "zenith-proxy" "25565";
+        zenith-alt = mkZenithInstance "zenith-alt" "25566";
+      };
 
       networking.firewall = {
-        allowedTCPPorts = [ 25565 ];
-        allowedUDPPorts = [ 25565 ];
+        allowedTCPPorts = [
+          25565
+          25566
+        ];
+        allowedUDPPorts = [
+          25565
+          25566
+        ];
       };
 
       sops = {
