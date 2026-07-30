10d852225d99d2956ffd0e223147dbad0595c82c
Colorman <foss@colorman.me>
2026-07-30 21:35:41 +0200

    services/nextcloud: switch from oidc_login to user_oidc

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -23,7 +23,7 @@
           inherit (pkgs.nextcloud33Packages.apps)
             end_to_end_encryption
             notify_push
-            oidc_login
+            user_oidc
             ;
         };
         config = {
@@ -36,29 +36,11 @@
           maintenance_window_start = 1;
           # OIDC settings
           lost_password_link = "disabled";
-          oidc_login_provider_url = "https://${authentikSecrets.domain}/application/o/nextcloud";
-          oidc_login_logout_url = "https://${nextcloudSecrets.hostname}";
-          oidc_login_button_text = "Log in with ColorCloud";
-          # oidc_login_hide_password_form = true;
-          oidc_login_attributes = {
-            id = "user_id";
-            name = "name";
-            mail = "email";
-            quota = "quota";
-            groups = "groups";
-          };
-          oidc_login_scope = "email profile openid nextcloud";
-          oidc_login_disable_registration = false;
-          oidc_create_groups = true;
-          oidc_login_code_challenge_method = "S256";
         };
         secrets = {
           instanceid = config.sops.secrets."services/nextcloud/instanceid".path;
           passwordsalt = config.sops.secrets."services/nextcloud/passwordsalt".path;
           secret = config.sops.secrets."services/nextcloud/secret".path;
-          # OIDC settings
-          oidc_login_client_id = config.sops.secrets."services/nextcloud/oidc_id".path;
-          oidc_login_client_secret = config.sops.secrets."services/nextcloud/oidc_secret".path;
         };
 
         configureRedis = true;
