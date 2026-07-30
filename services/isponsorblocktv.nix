d8b300a0b91fc20585824dfc933ca4ea47a43f93
Colorman <foss@colorman.me>
2026-07-30 20:59:59 +0200

    services/isponsorblocktv: pin image

--- services/isponsorblocktv.nix
+++ services/isponsorblocktv.nix
@@ -1,7 +1,7 @@
 {
   flake.nixosModules.services-isponsorblocktv = {
     virtualisation.oci-containers.containers.isponsorblocktv = {
-      image = "ghcr.io/dmunozv04/isponsorblocktv:latest";
+      image = "ghcr.io/dmunozv04/isponsorblocktv:v2.10.0@sha256:f2e4c7e57f6fcb490156759bcc79561ac594675354923a71f1f034a3fe39d7de";
       volumes = [
         "/var/lib/isponsorblocktv:/app/data"
       ];
