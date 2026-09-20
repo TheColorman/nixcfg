71be8b4df8fbe13e9727eceaf3de7accc3c20524
Colorman <foss@colorman.me>
2026-09-20 17:34:13 +0200

    hosts: add watcher

---
+++ hosts/watcher/disko-config.nix
@@ -0,0 +1,37 @@
+{
+  flake.nixosModules.watcher-disko-config =
+    { lib, ... }:
+    {
+      disko.devices = {
+        disk = {
+          main = {
+            device = "/dev/disk/by-id/nvme-eui.0025388291b48942";
+            type = "disk";
+            content = {
+              type = "gpt";
+              partitions = {
+                ESP = {
+                  size = "500M";
+                  type = "EF00";
+                  content = {
+                    type = "filesystem";
+                    format = "vfat";
+                    mountpoint = "/boot";
+                    mountOptions = [ "umask=0077" ];
+                  };
+                };
+                root = {
+                  size = "100%";
+                  content = {
+                    type = "filesystem";
+                    format = "ext4";
+                    mountpoint = "/";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+}
